057b174fd79a14846a264f44a566006272f346c0
mick murphy <mickm15@hotmail.com>
2018-01-28 18:49:07 +0000

    Lis Figs

--- js/Trig02.js
+++ js/Trig02.js
@@ -7,30 +7,29 @@ window.onload = function ()
 
     var centerY = height * 0.5,
         centerX = width * 0.5,
-        baseRadius = 30,
-        offset = height * 0.4,
-        speed = 0.01,
-        angle = 0;
+        xRadius = 120,
+        yRadius = 200,
+        xAngle = 0,
+        yAngle = 0,
+        xSpeed = 0.1,
+        ySpeed = 0.131,
+        x, y;
 
     Render();
 
     function Render()
     {
-        var y = centerY + Math.sin(angle) * offset;
-        var x = centerX + Math.cos(angle) * offset;
+        context.clearRect(0, 0, width,height);
 
-        var radius = (baseRadius) + Math.sin(angle) * offset;
+        x = centerX + Math.cos(xAngle) * xRadius;
+        y = centerY + Math.sin(yAngle) * yRadius;
 
-        context.clearRect(0,0,width,height);
         context.beginPath();
-
-        context.arc(x, y, Math.abs(radius), 0, Math.PI * 2, false);
+        context.arc(x, y, 20, 0, Math.PI * 2, false);
         context.fill();
 
-        angle += speed;
-
-        console.log("angle = " + angle);
-        console.log("radius = " + radius);
+        xAngle += xSpeed;
+        yAngle += ySpeed;
 
         requestAnimationFrame(Render);
     }
